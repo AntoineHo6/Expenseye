46f20eacaff37c7ca93ff27fcb9753ed8bedb9f9
Antoine Ho <34996565+AntoineHo6@users.noreply.github.com>
2020-05-27 12:29:15 -0400

    Fixed transportation lang problem

--- lib/Resources/Strings.dart
+++ lib/Resources/Strings.dart
@@ -3,7 +3,7 @@ class Strings {
 
   // * EXPENSE categories EN
   static const String foodEN = 'Food';
-  static const String transportationEN = 'Transport';
+  static const String transportationEN = 'Transportation';
   static const String shoppingEN = 'Shopping';
   static const String entertainmentEN = 'Entertainment';
   static const String activityEN = 'Activity';
@@ -15,7 +15,7 @@ class Strings {
   static const String otherExpensesEN = 'Other Expenses';
   // * EXPENSE categories FR
   static const String foodFR = 'Nourriture';
-  static const String transportationFR = 'Transportation';
+  static const String transportationFR = 'Transport';
   static const String shoppingFR = 'Magazinage';
   static const String entertainmentFR = 'Divertissement';
   static const String activityFR = 'Activité';

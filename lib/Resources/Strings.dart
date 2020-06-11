822bb4811ff14c3c80e0444d3feebdcb3970fd3c
Antoine Ho <34996565+AntoineHo6@users.noreply.github.com>
2020-06-10 23:28:35 -0400

    version upgrade

--- lib/Resources/Strings.dart
+++ lib/Resources/Strings.dart
@@ -1,5 +1,5 @@
 class Strings {
-  static const String versionNumber = '1.4.1';
+  static const String versionNumber = '1.4.2';
 
   // * EXPENSE categories EN
   static const String foodEN = 'Food';
